d19644e2730d9f604b875c65f6e21b7094320a06
Zsolt Ero <zsolt.ero@gmail.com>
2018-08-26 10:52:40 +0200

    md

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='gittag git tag',
     name='gittag',
